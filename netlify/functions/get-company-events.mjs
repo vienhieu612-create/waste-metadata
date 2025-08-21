031e1ab3f3dcf2601ed5989b14abe75a073633b6
vienhieu612-create <vienhieu612@gmail.com>
2025-08-22 00:32:28 +0800

    新增日志打印
    新增日志打印

--- netlify/functions/get-company-events.mjs
+++ netlify/functions/get-company-events.mjs
@@ -19,6 +19,7 @@ export default async function handler(event, context) {
   }
 
   try {
+    console.log(JSON.stringif(event))
     if (event.httpMethod !== 'GET') {
       return new Response(JSON.stringify({ error: '只支持GET请求' }), {
         status: 405,
